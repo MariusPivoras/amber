c1b520e4a0bb19253b35c4d4ca5002a41f69efd5
Marius <Marius@10.0.1.9>
2018-10-08 20:54:27 +0300

    Amber class

---
+++ GintarasClass.m
@@ -0,0 +1,11 @@
+classdef GintarasClass
+   properties
+      class
+      kiekis=0
+   end
+   methods (Static)
+      function suma = klasesKiekis(kiek)
+         suma = kiek + 1;
+      end
+   end
+end
