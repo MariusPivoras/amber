a0628967a69895aaf81dc0146dfd3702f052e346
User <User@10.0.1.8>
2018-10-09 18:48:43 +0300

    Veikiantis nuskaitymas

--- Nuskaitymas.m
+++ Nuskaitymas.m
@@ -1,6 +1,6 @@
 %------------------------------------------------------
 %Gintarø rûðiavimas
-%Modifikuota: 2018-10-06
+%Modifikuota: 2018-10-09
 %------------------------------------------------------
 
 global vid;
@@ -12,14 +12,11 @@ close all;
 %Panaikita serial porto konekcijas
 delete(instrfindall); 
 
-% stop(vid);
-% fclose(s);
-
 %------------------------------------------------------
 %-----COM porto apraðymas klasiø iðvedimui pradþia-----
 %------------------------------------------------------
 
-s=serial('COM1', 'Baudrate',38400); 
+s=serial('COM3', 'Baudrate',38400); 
 fopen(s);
 % fwrite(s,41);
 
@@ -31,31 +28,30 @@ fopen(s);
 %-----PointGrey kameros apraðymas pradþia-----
 %---------------------------------------------
 
-% vid = videoinput('pointgrey', 1, 'RGB32_640x480');
+vid = videoinput('pointgrey');
 
-% %src = getselectedsource(vid);
+% src = getselectedsource(vid);
  
-% vid.FramesPerTrigger = 1;
+vid.FramesPerTrigger = 1;
 
-% % src.BrightnessMode = 'Manual';
-% % src.ExposureMode = 'Manual';
-% % src.GainMode = 'Manual';
-% % src.PanMode = 'Manual';
-% % src.Brightness = 140;
-% % src.Exposure = 7;
-% % src.Gain = 0;
+% src.BrightnessMode = 'Manual';
+% src.ExposureMode = 'Manual';
+% src.GainMode = 'Manual';
+% src.PanMode = 'Manual';
+% src.Brightness = 140;
+% src.Exposure = 7;
+% src.Gain = 0;
  
-% set(vid, 'FramesPerTrigger', 1);
-% set(vid, 'TriggerRepeat', Inf);
-% triggerconfig(vid, 'immediate');%'manual');
+set(vid, 'FramesPerTrigger', 1);
+set(vid, 'TriggerRepeat', Inf);
+triggerconfig(vid, 'immediate');%'manual');
+%  vid.TimerFcn = {'stop'};
 
-% start(vid)
+start(vid)
 
 %-------------------------------------------
 %-----PointGrey kameros apraðymas galas-----
 %-------------------------------------------
-
-
 
 tmpLine = '------------------------------------------------------------------------------ \n';
 fprintf(tmpLine);
@@ -63,47 +59,52 @@ fprintf( 1, '|                  Pradedamas gintarø rûðiavimo procesas           
 
 amberNr=0;
 class=0;
-classNr=10;
+classNr=5;
 classM=2; % Klasës generavimo metodas
-
 
 % Klasiø konstruktorius
 for i=1:1:classNr
     Gintaras(i) = GintarasClass();
 end
 
-
 fprintf(tmpLine);
 
-tMain=tic;
-
-timeInterval = 5;
+timeInterval = 60;
 tMainElapsed = 0;
-
+pause
+tMain=tic;
+Im=[];
 
 while tMainElapsed < timeInterval
     
     tIner = tic;
-    
-    amberNr=amberNr+1;
-    % Im= peekdata(vid,1);
-    I=imread('C:\Users\Marius\Desktop\Magistrinis_darbas\MatLab\Gintaru_foto\Image.1.39.1.jpg');
-    switch classM
-        case 1
-            class=classL(class,classNr);
-        otherwise
-            class=classR(classNr);
-    
+    Im=peekdata(vid,1);
+
+    if size(Im,1)~=0 % Tikrina ar yra vaizdas
+      
+        amberNr=amberNr+1;
+%      imshow(Im)
+%      I=imread('C:\Users\Marius\Desktop\Magistrinis_darbas\MatLab\Gintaru_foto\Image.1.39.1.jpg');
+        switch classM
+            case 1
+                class=classL(class,classNr);
+            otherwise
+                class=classR(classNr);
+        end
+
+        Gintaras(class).kiekis = GintarasClass.klasesKiekis(Gintaras(class).kiekis);
+
+        fwrite(s,class,'int8');
+        tInerElapsed = toc(tIner);
+        fprintf('| Gintaras nr.: %2d | Klasë: %2d | Viso klasëje: %2d | Laikas: %2.8f sek. |\n', amberNr, class, Gintaras(class).kiekis, tInerElapsed);
+        fprintf(tmpLine);
+        
+        flushdata(vid, 'all'); % pasalina vaida is kameros
     end
     
-    Gintaras(class).kiekis = GintarasClass.klasesKiekis(Gintaras(class).kiekis);
+    Im=[]; % pasalina vaizda
     
-    % fwrite(s,class,'int8');
-    tInerElapsed = toc(tIner);
-    fprintf('| Gintaras nr.: %2d | Klasë: %2d | Viso klasëje: %2d | Laikas: %2.8f sek. |\n', amberNr, class, Gintaras(class).kiekis, tInerElapsed);
-    fprintf(tmpLine);
-    
-    tMainElapsed = toc(tMain);    
+    tMainElapsed = toc(tMain);
    
 end
 
@@ -120,6 +121,7 @@ function class=classL(class, classNr)
     
 end
 
+
 %---------------------------
 %-----Klasë atsitiktinë-----
 %---------------------------
@@ -128,8 +130,5 @@ function class=classR(classNr)
    class = randi([1 classNr],1);
     
 end
-
-%flushdata(vid);    
-%stop(vid);fclose(s)
 
 
